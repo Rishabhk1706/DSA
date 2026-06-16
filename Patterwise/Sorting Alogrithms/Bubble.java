c881903cbf249058bd69f6f271d8d5b3fe74a62b
Rishabhk1706 <rishabhk1706@gmail.com>
2026-06-16 23:37:48 +0530

    Bubble Sort optimize

--- Patterwise/Sorting Alogrithms/Bubble.java
+++ Patterwise/Sorting Alogrithms/Bubble.java
@@ -3,13 +3,16 @@ public class Bubble {
         int[] arr = {13, 46, 24, 52, 20, 9};
         int n = arr.length;
         for(int i=n-1;i>=1;i--){
+            boolean swap = false;
             for(int j=0;j<i;j++){
                 if(arr[j]>arr[j+1]){
+                    swap = true;
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
                 }
             }
+            if(!swap) break;
         }
         for(int i=0;i<n;i++){
             System.out.println(arr[i]);
